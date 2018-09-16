8b096635a143c73a3573dd11121cf5a704e79d49
Tony Ni <tony.zt.ni@gmail.com>
2018-09-16 05:05:57 -0400

    Initial commit

---
+++ Assets/Scripts/Squirrel0_Move.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Squirrel0_Move : MonoBehaviour
+{
+    private int loopCount;
+    private int upDown;
+    private float speed;
+    public Text displayLoops;
+
+    // Use this for initialization
+    void Start()
+    {
+        loopCount = 0;
+        upDown = 0;
+        speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (loopCount % 100 == 0)
+        {
+            GoUpDown();
+            upDown++;
+        }
+        loopCount++;
+        displayLoops.text = "Loops: " + loopCount.ToString();
+    }
+
+    void GoUpDown()
+    {
+        if (upDown % 3 == 0)
+        {
+            gameObject.transform.Translate(0, 10.0f, 0);
+        }
+        else if (upDown % 3 == 2)
+        {
+            gameObject.transform.Translate(0, -10.0f, 0);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseDown(0))
+        {
+
+        }
+    }
+}
